a01dd8d6c187a9113e320e8e35c8d56c9dc5b3c5
Chris Xiao <xchris80519@gmail.com>
2025-06-06 17:25:11 +0800

    Project Initialization

---
+++ src/main/java/com/cja101g4/shakemate/ShakemateApplication.java
@@ -0,0 +1,13 @@
+package com.cja101g4.shakemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cja101g4/shakemate/ShakemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cja101g4.shakemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
