bf758644dfc61fe946dcf4cedb03fb3cae89b8a3
Chris Xiao <xchris80519@gmail.com>
2025-06-10 17:37:43 +0800

    TYPE: Feat   更新User, 及登入功能。

--- src/main/java/com/shakemate/ShakemateApplication.java
+++ src/main/java/com/shakemate/ShakemateApplication.java
@@ -2,7 +2,11 @@ package com.shakemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.shakemate.user.dao")
+@EntityScan(basePackages = "com.shakemate.user.model")
 @SpringBootApplication
 public class ShakemateApplication {
 
