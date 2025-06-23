cce887982f438808fdbb2e1776d7a61b79385397
Carter <whiteslime891201@gmail.com>
2025-06-23 16:20:09 +0800

    註解掉AI相關程式碼(方便無金鑰時測試)，路徑修改完成，增加shakemate_homepage.html，複製內容到index.html。

--- src/main/java/com/shakemate/IndexController.java
+++ src/main/java/com/shakemate/IndexController.java
@@ -8,13 +8,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+	// 首頁設計完成檔案，各位可參考並直接做修改： shakemate_homepage.html
+	
     @GetMapping("/")
     public String MyMethod() {
         return "index"; // --->  src\main\resources\templates\index1.html
+//        return "shakemate_homepage";
     }
+    
     @GetMapping("/login")
     public String showLoginForm() {
         return "login"; // 對應 resources/templates/login.html
     }
+    
+    // 用來導向客服GPT頁面 index.html
+    @GetMapping("/shakemate/chat")
+    public String chatPage() {
+        return "front-end/servicecase/index";
+    }
+    
+
 
 }
