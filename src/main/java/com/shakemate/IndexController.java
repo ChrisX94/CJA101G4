011119615ce2d37a5cfdbcb638a00fd80f394472
Carter <whiteslime891201@gmail.com>
2025-06-26 01:19:52 +0800

    Modify:既有功能需求調整的修改。

--- src/main/java/com/shakemate/IndexController.java
+++ src/main/java/com/shakemate/IndexController.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-	// 首頁設計完成檔案，各位可參考並直接做修改： shakemate_homepage.html
+	// 首頁設計完成檔案，各位可參考並直接做修改： index.html
 
 	@GetMapping("/")
 	public String MyMethod() {
 		return "index"; // ---> src\main\resources\templates\index1.html
-//        return "shakemate_homepage";
 	}
 
 	@GetMapping("/login")
@@ -21,10 +20,10 @@ public class IndexController {
 		return "login"; // 對應 resources/templates/login.html
 	}
 
-	// 用來導向客服GPT頁面 index.html
+	// 用來導向客服GPT頁面 sindex.html
 	@GetMapping("/shakemate/chat")
 	public String chatPage() {
-		return "front-end/servicecase/index";
+		return "front-end/servicecase/sindex";
 	}
 
     // 用來導向後台頁面 manage.html
