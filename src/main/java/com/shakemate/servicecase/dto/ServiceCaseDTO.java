1be40f7cb9ffefccfc35645aa810a600c7220642
Carter <whiteslime891201@gmail.com>
2025-07-02 21:25:44 +0800

    Modify

--- src/main/java/com/shakemate/servicecase/dto/ServiceCaseDTO.java
+++ src/main/java/com/shakemate/servicecase/dto/ServiceCaseDTO.java
@@ -4,18 +4,22 @@ package com.shakemate.servicecase.dto;
 import java.sql.Timestamp;
 
 public class ServiceCaseDTO {
-    private Integer caseId;
-    private Integer userId;
-    private Integer admId;
-    private Integer caseTypeId;
-    private String caseTypeName;   // 額外欄位：直接顯示 CaseType 的名稱
-    private Timestamp createTime;
-    private Timestamp updateTime;
-    private String title;
-    private String content;
-    private Integer caseStatus;
+	private Integer caseId;
+	private Integer userId;
+	private Integer admId;
+	private Integer caseTypeId;
+	private String caseTypeName; // 額外欄位：直接顯示 CaseType 的名稱
+	private Timestamp createTime;
+	private Timestamp updateTime;
+	private String title;
+	private String content;
+	private Integer caseStatus;
+	/** 新增 email 欄位 */
+	private String email;
 
-    public ServiceCaseDTO() {}
+	
+	public ServiceCaseDTO() {
+	}
 
 	public Integer getCaseId() {
 		return caseId;
@@ -97,5 +101,13 @@ public class ServiceCaseDTO {
 		this.caseStatus = caseStatus;
 	}
 
-    
+	// 加上這兩個方法：
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
