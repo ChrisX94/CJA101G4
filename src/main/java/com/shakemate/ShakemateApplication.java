07eaf51819374a94a668e62e058e0b12338ae8b3
hys <hys.tibame@gmail.com>
2025-07-07 02:29:48 +0800

    Fix: 確認能啟動

--- src/main/java/com/shakemate/ShakemateApplication.java
+++ src/main/java/com/shakemate/ShakemateApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.shakemate")
 @EntityScan(basePackages = "com.shakemate")
 @SpringBootApplication(scanBasePackages = "com.shakemate")
-
 public class ShakemateApplication {
 
 	public static void main(String[] args) {
