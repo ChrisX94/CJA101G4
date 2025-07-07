15f6af734dbcebcadcabbd4f8890a48fe5a4176a
Chris Xiao <xchris80519@gmail.com>
2025-07-08 00:24:44 +0800

    Fix, Feat:   fix the bug in addview() function.   add a new function of connecting to chat room

--- src/main/java/com/shakemate/shshop/dao/ShShopRepository.java
+++ src/main/java/com/shakemate/shshop/dao/ShShopRepository.java
@@ -2,6 +2,7 @@ package com.shakemate.shshop.dao;
 
 import com.shakemate.shshop.model.ShProd;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,60 +10,62 @@ import java.util.List;
 
 public interface ShShopRepository extends JpaRepository<ShProd, Integer> {
 
-@Query("""
-SELECT DISTINCT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
-""")
+    @Query("""
+            SELECT DISTINCT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+            """)
     List<ShProd> findAllWithPics();
 
-// 找待審核的商品
+    // 找待審核的商品
     @Query("""
-SELECT DISTINCT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
- WHERE p.prodStatus = 0
-""")
-    List<ShProd> findAllWithPendingApproval ();
+            SELECT DISTINCT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+             WHERE p.prodStatus = 0
+            """)
+    List<ShProd> findAllWithPendingApproval();
 
-@Query("""
-SELECT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
- WHERE p.prodId = ?1
-""")
+    @Query("""
+            SELECT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+             WHERE p.prodId = ?1
+            """)
     ShProd getByID(Integer id);
 
-@Query("""
-SELECT DISTINCT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
- WHERE p.shProdType.prodTypeId = :typeId
-""")
+    @Query("""
+            SELECT DISTINCT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+             WHERE p.shProdType.prodTypeId = :typeId
+            """)
     List<ShProd> getByType(@Param("typeId") Integer typeId);
 
-@Query("""
-SELECT DISTINCT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
- WHERE p.user.userId = :userId
-""")
+    @Query("""
+            SELECT DISTINCT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+             WHERE p.user.userId = :userId
+            """)
     List<ShProd> getByUserId(@Param("userId") Integer userId);
 
-@Query("""
-SELECT DISTINCT p FROM ShProd p
- LEFT JOIN FETCH p.prodPics
- LEFT JOIN FETCH p.shProdType
- WHERE p.user.userId = :userId
- AND(
-    p.prodName LIKE CONCAT('%', :keyStr, '%')
-    OR p.prodBrand LIKE CONCAT('%', :keyStr, '%')
-    OR p.prodContent LIKE CONCAT('%', :keyStr, '%')
-    )
-""")
-List<ShProd> getByUserAndKeyStr(@Param("userId") Integer userId, @Param("keyStr") String keyStr);
+    @Query("""
+            SELECT DISTINCT p FROM ShProd p
+             LEFT JOIN FETCH p.prodPics
+             LEFT JOIN FETCH p.shProdType
+             WHERE p.user.userId = :userId
+             AND(
+                p.prodName LIKE CONCAT('%', :keyStr, '%')
+                OR p.prodBrand LIKE CONCAT('%', :keyStr, '%')
+                OR p.prodContent LIKE CONCAT('%', :keyStr, '%')
+                )
+            """)
+    List<ShProd> getByUserAndKeyStr(@Param("userId") Integer userId, @Param("keyStr") String keyStr);
 
-
+    @Query("UPDATE ShProd p SET p.prodViews = p.prodViews + 1 WHERE p.prodId = :id")
+    @Modifying
+    void incrementViews(@Param("id") Integer id);
 
 }
 
