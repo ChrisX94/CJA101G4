b4f1f8003c1b6c324be3bd18eafa97a6ffb920b7
Chris Xiao <xchris80519@gmail.com>
2025-07-09 03:23:12 +0800

    Fix: add links in back-end pages,

--- src/main/java/com/shakemate/ShakemateApplication.java
+++ src/main/java/com/shakemate/ShakemateApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 
 @EnableJpaRepositories(basePackages = "com.shakemate")
 @EntityScan(basePackages = "com.shakemate")
 @SpringBootApplication(scanBasePackages = "com.shakemate")
-@MapperScan("com.shakemate.activity.mapper")
+
 public class ShakemateApplication {
 
 	public static void main(String[] args) {
